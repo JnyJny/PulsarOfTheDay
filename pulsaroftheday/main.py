55b44b77ad73ff4a90419ca093e06d98e630e48a
JnyJny <erik.oshaughnessy@gmail.com>
2021-05-26 14:47:31 -0500

    refactor: took stuff out of main and put in the catalog

--- pulsaroftheday/main.py
+++ pulsaroftheday/main.py
@@ -29,29 +29,10 @@ app_config_path.mkdir(parents=True, exist_ok=True)
 def log_pulsars(df: pd.DataFrame, dropna: bool = False) -> None:
     """Log a pulsar to the console."""
 
-    pulsars = df[Pulsar.keys()].copy()
-
-    if dropna:
-        pulsars.dropna(inplace=True)
-
-    for values in pulsars.itertuples(index=False):
-        pulsar = Pulsar(*values)
+    for pulsar in Pulsar.from_dataframe(df):
         logger.info(f"RECORD B={pulsar.PSRB} J={pulsar.PSRJ}")
         for n, line in enumerate(str(pulsar).splitlines()):
             logger.log("PULSAR", line)
-
-
-def dump_pulsars(df: pd.DataFrame, keys: List[str] = None) -> None:
-    """Dumps a CSV formated list of pulsars.
-
-    :param df: pandas.DataFrame
-    :param keys: optional List[str]
-    """
-    keys = keys or df.columns
-    keys.insert(0, "INDEX")
-    print(":".join(keys))
-    for record in df[keys].itertuples(name="Pulsar_Dump"):
-        print(":".join([str(f) for f in record]))
 
 
 @cli.callback()
@@ -59,23 +40,30 @@ def main_command(
     ctx: typer.Context,
     verbose: int = typer.Option(0, "--verbose", "-v", count=True),
     path: Path = typer.Option(
-        None, "--csv-path", "-p", help="Path to CSV format pulsar data."
+        None,
+        "--csv-path",
+        "-p",
+        help="Path to CSV format pulsar data.",
     ),
 ) -> None:
 
     global catalog
 
     logger.remove()
-    logger_config = {
-        "colorize": True,
-        "format": "<green>{time}</green>|<level>{level:<8}</level>|{message}",
-        "level": {1: "INFO", 2: "DEBUG"}.get(verbose, "INFO"),
-    }
-    logger.add(sys.stdout, **logger_config)
-    logger.level("PULSAR", no=38, color="<blue>", icon="✪")
+    if verbose:
+        logger_config = {
+            "colorize": True,
+            "format": "<green>{time}</green>|<level>{level:<8}</level>|{message}",
+            "level": {
+                1: "SUCCESS",
+                2: "INFO",
+                3: "DEBUG",
+            }.get(verbose, "INFO"),
+        }
+        logger.add(sys.stdout, **logger_config)
+        logger.level("PULSAR", no=38, color="<blue>", icon="✪")
 
     try:
-
         csv_path = (Path(path or app_config_path) / "pulsars.csv").resolve()
 
         logger.info(f"Config path: {app_config_path}")
@@ -83,87 +71,67 @@ def main_command(
         catalog = PulsarCatalog(csv_path)
 
         logger.success(f"Pulsars in catalog: {len(catalog.dataframe)}")
-
-        df = catalog.dataframe.dropna(subset=["RAJ", "DECJ", "period", "pdot"])
-
-        logger.success(f"Good pulsar candidates: {len(df)}")
+        logger.success(f"Plottable pulsars:  {len(catalog.plottable)}")
 
         logger.info(f"Catalog data @ {catalog.csv_path.resolve()}")
 
-        if not csv_path.exists():
-            catalog.write()
+        if not catalog.csv_path.exists():
+            catalog.save()
             logger.info(f"Wrote CSV data to improve future startup times.")
 
     except Exception as error:
-        logger.error(f"{error} on startup")
+        logger.error(f"{error} on startup.")
         raise typer.Exit()
+
+
+@cli.command(name="init")
+def init_subcommand(
+    ctx: typer.Context,
+    force: bool = typer.Option(
+        False,
+        "--force",
+        "-f",
+        is_flag=True,
+        help="Force re-initialize pulsar database from source data.",
+    ),
+) -> None:
+
+    catalog.initialize(force=force)
 
 
 @cli.command(name="list")
 def list_subcommand(
     ctx: typer.Context,
     pulsar_name: str = typer.Option(
-        None, "--pulsar", "-p", help="Long listing for the named pulsar."
+        None,
+        "--pulsar-name",
+        "-n",
+        help="Either a B or J name for a given pulsar.",
     ),
-    all_pulsars: bool = typer.Option(
+    plottable: bool = typer.Option(
         False,
-        "--all",
-        "-a",
-        is_flag=True,
-        help="List all pulsars in the catalog.",
+        "--plottable",
+        "-p",
+        help="List only plottable pulsars.",
     ),
-    long_listing: bool = typer.Option(
+    full_dump: bool = typer.Option(
         False,
-        "--long-fmt",
+        "--long-listing",
         "-l",
-        help="List pulsars in CSV format.",
+        help="List all columns in the pulsar database.",
     ),
 ) -> None:
-    """List pulsars in the catalog.
+    """List pulsars in the catalog in CSV format."""
 
-    By default, will list "tweetable" records which are those pulsar
-    records with valid data for PSRB, PSRJ, RAJ, DECJ, F0, F1, and DM.
-
-    When the user specifies --all, a long list in CSV format is printed
-    to stdout for all records in the catalog.
-
-    When the user specifies a specific pulsar with the `--pulsar` option,
-    it will be printed in CSV format to stdout.
-    """
-
-    if all_pulsars and pulsar_name:
-        logger.error("Mutually exclusive options: --all, --pulsar")
-        raise typer.Exit()
+    df = catalog.plottable if plottable else catalog.dataframe
 
     if pulsar_name:
-        # XXX print tweetable form of pulsar if possible/switch
-        logger.info(f"Looking up {pulsar_name} in the catalog")
-        df = catalog.dataframe[catalog.dataframe.NAME == pulsar_name]
-        logger.info(f"Found {len(df)} matching records.")
-        log_pulsars(df)
-        logger.success("List pulsar by name")
-        return
+        df = df[catalog.dataframe.NAME == pulsar_name]
 
-    if all_pulsars:
-        logger.info(f"Listing {len(catalog.dataframe)} Pulsars")
-        dump_pulsars(catalog.dataframe)
-        return
+    if not full_dump:
+        df = df[Pulsar.keys()]
 
-    # Narrow the catalog to entries with only these keys
-    df = catalog.dataframe[Pulsar.keys()].dropna()
-
-    if long_listing:
-        dump_pulsars(df)
-    else:
-        log_pulsars(df)
-
-    # for record in df.itertuples(index=False, name="Pulsar"):
-    #     try:
-    #         pulsar = Pulsar(*record)
-    #         dump_pulsars(pulsar)
-    #     except Exception as error:
-    #         logger.error(f"{error} for record: {record}")
-    #         raise
+    print(df.to_csv(index=True, index_label="INDEX"))
 
 
 @cli.command(name="tweet")
@@ -182,51 +150,31 @@ def tweet_subcommand(
         "-p",
         help="Specific pulsar name to tweet.",
     ),
-    plots_path: Path = typer.Option(
+    tweet_archive_path: Path = typer.Option(
         None,
-        "--plots-path",
-        "-P",
-        help="Path to save generated plots.",
+        "--tweet-archive-path",
+        "-t",
+        help="Path where tweet text and plots are written.",
     ),
 ):
     """Tweet a pulsar record and accompanying plot."""
 
-    plots_path = Path(plots_path or app_config_path / "plots").resolve()
+    tweets_path = Path(tweet_archive_path or app_config_path / "tweets").resolve()
 
-    plots_path.mkdir(parents=True, exist_ok=True)
+    tweets_path.mkdir(parents=True, exist_ok=True)
 
     today = datetime.now().isoformat().partition("T")[0]
 
-    df = catalog.dataframe.dropna(subset=["period", "pdot", "DECJ", "RAJ"])
+    sample = catalog.random_pulsar_population(include_name=pulsar_name)
 
-    logger.info(f"Pulsars matching critera: {len(df)}")
+    sample = sample.dropna(subset=["NAME", "DECJ", "RAJ", "period", "pdot"])
 
-    sample = df.sample(len(df))  # random shuffle
-
-    # If the user isn't requesting a particular pulsar by name,
-    # we pick the first pulsar in `sample` as the "target" pulsar.
-    #
-    # When the user wants a particular pulsar, we search for it
-    # and then append `sample` to "target" DataFrame and then
-    # drop duplicates, keeping the first one. This shuffles the
-    # target pulsar to top of the sample DataFrame without a duplicate.
-    # The duplicate would just cause two draws of the pulsar in the
-    # plotting phase, so it's not entirely necessary.
-
-    if pulsar_name:
-        target = df[df.CNAME.str.contains(pulsar_name, regex=False)]
-
-        logger.info(f"{pulsar_name} matched {len(target)} records")
-
-        if target.empty:
-            logger.error(f"No pulsar matches '{pulsar_name}'")
-            raise typer.Exit()
-
-        sample = target.append(sample).drop_duplicates()
+    logger.info(f"Pulsars matching tweeting critera: {len(sample)}")
 
     # The first row in the dataframe is the target
 
     sample.loc[sample.index[0], "color"] = "red"
+
     pulsar = Pulsar(*sample.iloc[0][Pulsar.keys()].values)
 
     if dryrun:
@@ -234,11 +182,20 @@ def tweet_subcommand(
 
     log_pulsars(sample.head(1))
 
-    plotfile = (plots_path / f"{today}.png").resolve()
+    tweet_text = tweets_path / f"{today}.text"
 
-    generate_pdot_skymap_plots(sample, plotfile)
+    try:
+        print(f"{pulsar:tweet}", file=tweet_text.open("w"))
+    except Exception as error:
+        logger.error("{error} writing tweet to {tweet_text}")
+        raise typer.Exit()
+    logger.success(f"Tweet text written to {tweet_text}")
 
-    logger.success(f"Plot @ {plotfile}")
+    tweet_plot = (tweets_path / f"{today}.png").resolve()
+
+    generate_pdot_skymap_plots(sample, tweet_plot)
+
+    logger.success(f"Tweet plot written to {tweet_plot}")
 
     if dryrun:
         logger.info("DRY RUN COMPLETE, nothing tweeted.")
@@ -266,10 +223,10 @@ def tweet_subcommand(
     except Exception as error:
         logger.error(f"teepy.API {error}")
 
-    logger.info(f"Tweeting {pulsar.NAME} and {plotfile}")
+    logger.info(f"Tweeting {pulsar.NAME} and {tweet_plot.name}")
 
     try:
-        tweet.update_with_media(filename=plotfile, status=str(pulsar))
+        tweet.update_with_media(filename=tweet_plot, status=pulsar.tweet)
     except Exception as error:
         logger.error(f"update_with_media failed: {error}")
         raise typer.Exit()
